8281684bdb417892372ee2f36e596d8866a86c93
Steve Konves <stephen.konves@gmail.com>
2016-09-27 14:26:05 -0700

    Merge release build output

--- merge.cmd
+++ merge.cmd
@@ -1,9 +1,9 @@
 nuget install ILMerge -Version 2.14.1208 -OutputDirectory tools
 
 .\tools\ILMerge.2.14.1208\tools\ilmerge.exe ^
-	.\src\SilverRock.Lazuli\bin\Debug\SilverRock.Lazuli.exe ^
-	.\src\SilverRock.Lazuli\bin\Debug\Microsoft.ServiceBus.dll ^
-	.\src\SilverRock.Lazuli\bin\Debug\NDesk.Options.dll ^
-	.\src\SilverRock.Lazuli\bin\Debug\Newtonsoft.Json.dll ^
-	.\src\SilverRock.Lazuli\bin\Debug\SilverRock.AzureTools.dll ^
+	.\src\SilverRock.Lazuli\bin\Release\SilverRock.Lazuli.exe ^
+	.\src\SilverRock.Lazuli\bin\Release\Microsoft.ServiceBus.dll ^
+	.\src\SilverRock.Lazuli\bin\Release\NDesk.Options.dll ^
+	.\src\SilverRock.Lazuli\bin\Release\Newtonsoft.Json.dll ^
+	.\src\SilverRock.Lazuli\bin\Release\SilverRock.AzureTools.dll ^
 	/out:lazuli.exe
