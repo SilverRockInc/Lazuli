9ab74bdfb215bea244b1fb42b70f3533b4974038
Steve Konves <stephen.konves@gmail.com>
2016-09-27 16:47:23 -0700

    Fix line continuation

--- coverage.cmd
+++ coverage.cmd
@@ -1,12 +1,12 @@
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe `
-	-target:"vstest.console.exe" `
-	-filter:"+[*]* -[*.*Tests]*" `
-	-targetargs:".\tests\SilverRock.Lazuli.UnitTests\bin\Release\SilverRock.Lazuli.UnitTests.dll" `
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe ^
+	-target:"vstest.console.exe" ^
+	-filter:"+[*]* -[*.*Tests]*" ^
+	-targetargs:".\tests\SilverRock.Lazuli.UnitTests\bin\Release\SilverRock.Lazuli.UnitTests.dll" ^
 	-register:user
 
-.\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe `
-	--opencover `
+.\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe ^
+	--opencover ^
 	-i .\results.xml
